eb7210cc0b99fd46bed1bc3a361522e6668fe5a6
Dimamar <DimaMar1@yandex.ru>
2015-12-21 00:28:24 +0300

    добавлен прототи обработки циклов

---
+++ src/KIPer/MineLoop/Loop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MainLoop
+{
+    public class Loop
+    {
+        /// <summary>
+        /// Коллекция разделяемых ресурсов
+        /// </summary>
+        /// <remarks>
+        /// Такими ресурсами в цикле опроса могут быть порты
+        /// </remarks>
+        private IDictionary<string, LoopDescriptor> lockers;
+
+        private IDictionary<string, CancellationTokenSource> cancelThreadCollection;
+    }
+}
---
+++ src/KIPer/MineLoop/LoopDescriptor.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace MainLoop
+{
+    class LoopDescriptor
+    {
+        private CancellationToken cancel;
+        private object locker;
+        private Queue<Action<object>> importantActions;
+        private Queue<Action<object>> middleActions;
+        private Queue<Action<object>> unimportantActions;
+
+        private int _importantCount = 0;
+        private int _middleCount = 0;
+        private int _unimportantCount = 0;
+
+        public LoopDescriptor(object locker, CancellationToken cancel)
+        {
+            this.locker = locker;
+            this.cancel = cancel;
+            importantActions = new Queue<Action<object>>();
+            middleActions = new Queue<Action<object>>();
+            unimportantActions = new Queue<Action<object>>();
+
+        }
+
+        public void AddImportant(Action<object> action)
+        {
+            lock (importantActions)
+            {
+                importantActions.Enqueue(action);
+                _importantCount++;
+            }
+        }
+
+        public void AddMiddle(Action<object> action)
+        {
+            lock (middleActions)
+            {
+                middleActions.Enqueue(action);
+                _middleCount++;
+            }
+        }
+
+        public void AddUnimportant(Action<object> action)
+        {
+            lock (unimportantActions)
+            {
+                unimportantActions.Enqueue(action);
+                _unimportantCount++;
+            }
+        }
+
+        internal Action<object> GetImportant()
+        {
+            if(_importantCount<=0)
+                return null;
+            Action<object> result;
+            lock (importantActions)
+            {
+                result = importantActions.Dequeue();
+                _importantCount--;
+            }
+            return result;
+        }
+
+    }
+}
