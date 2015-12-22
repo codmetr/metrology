c7f294b702f798a46bb3e3ca13fd1533116f5e8b
Dimamar <DimaMar1@yandex.ru>
2015-12-22 23:47:27 +0300

    обновление документации

--- src/KIPer/Drivers/PACE5000/PACEDriver.cs
+++ src/KIPer/Drivers/PACE5000/PACEDriver.cs
@@ -37,5 +37,19 @@ namespace PACESeries
                 throw new AnswerException(commandBase, answer);
             return result;
         }
+
+        int GetNumberCalibratinPoints(int channel)
+        {
+            var commandBase = string.Format(":CAL{0}:PRES:POIN", channel);
+            var command = string.Format("{0}?", commandBase);
+            var answer = SendCommand(command);
+            if (!answer.StartsWith(commandBase))
+                throw new AnswerException(commandBase, answer);
+            var stringValue = answer.Remove(0, commandBase.Length);
+            int result;
+            if(!int.TryParse(stringValue, out result))
+                throw new AnswerException(commandBase, answer);
+            return result;
+        }
     }
 }
