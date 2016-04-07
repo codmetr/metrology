516d3fcfa8fd317883b5dd4611ba1300c299e1b8
Dimamar <DimaMar1@yandex.ru>
2016-04-08 00:05:48 +0300

    попытка подключения по agilent

---
+++ src/KIPer/IEEE488Terminal/TerminalModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Agilent.TMFramework.Connectivity;
+namespace IEEE488Terminal
+{
+    public class TerminalModel
+    {
+        /// <summary>
+        /// Get a device descriptor
+        /// </summary>
+        /// <param name="boardID">Board number to which the device is connected</param>
+        /// <param name="pad">Primary address of the device</param>
+        /// <param name="sad">Secondary address of the device (0 if none)</param>
+        /// <param name="tmo">Timeout for the device (Txxx constants: see ibtmo reference in help)</param>
+        /// <param name="eot">1 to enable end-of-transmission EOI, 0 to disable EOI</param>
+        /// <param name="eos">0 to disable end-of-string termination, nonzero to enable (see ibeos)</param>
+        /// <returns>Status outcome</returns>
+        public bool TryConnect(int boardID, int pad, int sad, int tmo, int eot, int eos)
+        {
+            var desc = Ag488Wrap.ibdev(boardID, pad, sad, tmo, eot, eos);
+            return desc == 0;
+        }
+
+
+    }
+}
---
+++ src/KIPer/IEEE488UnitTest/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Agilent.TMFramework.Connectivity;
+
+namespace IEEE488UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int boardID=0;
+            int pad=16;
+            int sad=0;
+            int tmo=500;
+            int eot=1;
+            int eos=0;
+            var desc = Ag488Wrap.ibdev(boardID, pad, sad, tmo, eot, eos);
+            desc.ToString();
+        }
+    }
+}
