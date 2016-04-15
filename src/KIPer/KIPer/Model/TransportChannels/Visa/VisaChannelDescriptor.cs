f30a1e90420f817caf75e77450a386cb1fcab32f
Dimamar <DimaMar1@yandex.ru>
2016-04-16 00:05:01 +0300

    прототип описателя канала связи с ADTS

---
+++ src/KIPer/KIPer/Model/TransportChannels/Visa/VisaChannelDescriptor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KipTM.Model.TransportChannels
+{
+    public class VisaChannelDescriptor: ITransportChannelType
+    {
+        public static string KeyType = "Visa";
+        private VisaSettings _settings;
+
+        public VisaChannelDescriptor()
+        {
+            Name = "Канал VISA";
+            Key = KeyType;
+        }
+
+        public string Key { get; private set; }
+        public string Name { get; private set; }
+        public object Settings { get { return _settings; } }
+    }
+}
---
+++ src/KIPer/KIPer/Model/TransportChannels/Visa/VisaSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KipTM.Model.TransportChannels
+{
+    public class VisaSettings
+    {
+        public string Address { get; set; }
+    }
+}
