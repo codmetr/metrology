8d38c5372389b57d8259873004e9bf46a02c98a3
Dimamar <DimaMar1@yandex.ru>
2016-06-14 00:06:36 +0300

    Подготовлена база для отчета

---
+++ src/KIPer/Reports/ReportAdts/AdtsCommonReportData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReportAdts
+{
+    public class AdtsCommonReportData
+    {
+        public string ReportNumber { get; set; }
+
+        public string DeviceType { get; set; }
+
+        public string SerialNumber { get; set; }
+
+        public string ChannelRange { get; set; }
+
+        public string CheckDate { get; set; }
+
+        public string EthalonDeviceType { get; set; }
+
+        public string EthalonError { get; set; }
+
+        public string EthalonChannelRange { get; set; }
+
+        public string CheckMethod { get; set; }
+
+        public string TypeSeviceEquipement { get; set; }
+
+        public string AtmosphericPressure { get; set; }
+
+        public string Temperature { get; set; }
+    }
+}
---
+++ src/KIPer/Reports/ReportAdts/AdtsReportData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReportAdts
+{
+    public class AdtsReportData
+    {
+        public string Point { get; set; }
+
+        public string RealValue { get; set; }
+
+        public string ErrorValue { get; set; }
+
+        public string Tolerance { get; set; }
+
+        public string IsCorrect { get; set; }
+    }
+}
