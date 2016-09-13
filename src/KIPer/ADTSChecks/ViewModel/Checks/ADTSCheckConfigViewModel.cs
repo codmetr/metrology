4af30f37421c7eafb597b2f6ced49c26be9f40e4
Dimamar <DimaMar1@yandex.ru>
2016-09-13 23:50:51 +0300

    Рефакторинг. Убран лишний класс

--- src/KIPer/ADTSChecks/ViewModel/Checks/ADTSCheckConfigViewModel.cs
+++ src/KIPer/ADTSChecks/ViewModel/Checks/ADTSCheckConfigViewModel.cs
@@ -20,13 +20,13 @@ namespace KipTM.ViewModel.Checks
     public class ADTSCheckConfigViewModel : ViewModelBase, ICustomSettingsViewModel
     {
         private ADTSMethodParameters _customConf;
-        private ObservableCollection<ADTSPionViewModel> _points;
+        private ObservableCollection<ADTSPoint> _points;
         private double _newPressure;
         private double _newTolerance = 0.1;
         private ICommand _up;
         private ICommand _down;
         private ICommand _addPoint;
-        private ADTSPionViewModel _selectedPoint;
+        private ADTSPoint _selectedPoint;
 
 
         /// <summary>
@@ -35,9 +35,8 @@ namespace KipTM.ViewModel.Checks
         public ADTSCheckConfigViewModel(ADTSMethodParameters customConf)
         {
             _customConf = customConf;
-            _points = new ObservableCollection<ADTSPionViewModel>(_customConf.Points.Select(el => new ADTSPionViewModel(el, Up, Down)));
-            UpdatePoints(_points);
-
+            _points = new ObservableCollection<ADTSPoint>(_customConf.Points);
+            
             _up = new CommandWrapper((arg) => DoUp((int?)arg));
 
             _down = new CommandWrapper((arg) => DoDown((int?)arg));
@@ -48,12 +47,12 @@ namespace KipTM.ViewModel.Checks
         /// <summary>
         /// Коллекция точек
         /// </summary>
-        public ObservableCollection<ADTSPionViewModel> Points { get { return _points; } }
+        public ObservableCollection<ADTSPoint> Points { get { return _points; } }
 
         /// <summary>
         /// Выделенная точка
         /// </summary>
-        public ADTSPionViewModel SelectedPoint
+        public ADTSPoint SelectedPoint
         {
             get { return _selectedPoint; }
             set { Set(ref _selectedPoint, value); }
@@ -83,14 +82,6 @@ namespace KipTM.ViewModel.Checks
 
         public ICommand Down { get { return _down; } }
 
-        private void UpdatePoints(ObservableCollection<ADTSPionViewModel> points)
-        {
-            for (int i = 0; i < points.Count; i++)
-            {
-                points[i].Index = i;
-            }
-        }
-
         private void DoUp(int? index)
         {
             if (index == null)
@@ -105,8 +96,9 @@ namespace KipTM.ViewModel.Checks
             var val = _points[index.Value];
             _points.RemoveAt(index.Value);
             _points.Insert(index.Value - 1, val);
+            _customConf.Points.RemoveAt(index.Value);
+            _customConf.Points.Insert(index.Value - 1, val);
             SelectedPoint = val;
-            UpdatePoints(_points);
         }
 
         private void DoDown(int? index)
@@ -123,8 +115,9 @@ namespace KipTM.ViewModel.Checks
             var val = _points[index.Value];
             _points.RemoveAt(index.Value);
             _points.Insert(index.Value + 1, val);
+            _customConf.Points.RemoveAt(index.Value);
+            _customConf.Points.Insert(index.Value + 1, val);
             SelectedPoint = val;
-            UpdatePoints(_points);
         }
 
         private void DoAddPoint()
@@ -138,56 +131,23 @@ namespace KipTM.ViewModel.Checks
                 Tolerance = NewTolerance
             };
 
-            if (_points.Count == 0 || point.Pressure < _points.Last().Value.Pressure)
+            if (_points.Count == 0 || point.Pressure < _points.Last().Pressure)
             {
-                _points.Add(new ADTSPionViewModel(point, Up, Down));
+                _points.Add(point);
                 _customConf.Points.Add(point);
-                UpdatePoints(_points);
                 return;
             }
 
             int index = _points.Count - 1;
             for (int i = _points.Count - 1; i >= 0; i--)
             {
-                if (point.Pressure < _points[i].Value.Pressure)
+                if (point.Pressure < _points[i].Pressure)
                     break;
                 index = i;
             }
-            _points.Insert(index, new ADTSPionViewModel(point, Up, Down));
+            _points.Insert(index, point);
             _customConf.Points.Insert(index, point);
-            UpdatePoints(_points);
         }
 
-    }
-
-    public class ADTSPionViewModel
-    {
-        private readonly ADTSPoint _value;
-        private readonly ICommand _up;
-        private readonly ICommand _down;
-
-        public ADTSPionViewModel(ADTSPoint value, ICommand up, ICommand down)
-        {
-            _value = value;
-            _up = up;
-            _down = down;
-        }
-
-        public ADTSPoint Value
-        {
-            get { return _value; }
-        }
-
-        public int Index { get; set; }
-
-        public ICommand Up
-        {
-            get { return _up; }
-        }
-
-        public ICommand Down
-        {
-            get { return _down; }
-        }
     }
 }
