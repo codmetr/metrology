e98af7903bec85b665b6ff291710436ba5c94cab
Dimamar <DimaMar1@yandex.ru>
2016-12-11 16:50:03 +0300

    Перенос канала в отдельную сборку 50%

---
+++ src/KIPer/VisaChannel/TransportChannels/Visa/VisaSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KipTM.Model.TransportChannels
+{
+    public class VisaSettings
+    {
+        private string _address;
+
+        public string Address
+        {
+            get { return _address; }
+            set
+            {
+                _address = value;
+                AddressFull = string.Format("GPIB0::{0}", _address);
+            }
+        }
+
+        public string AddressFull { get; private set; }
+    }
+}
