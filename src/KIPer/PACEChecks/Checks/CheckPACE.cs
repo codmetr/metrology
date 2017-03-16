2b422adfa60a5e589f358abe248348df1edd4796
codmetr <cod.metr@yandex.ru>
2017-03-16 23:56:31 +0300

    заготовка для поверки/аттестации PACE

---
+++ src/KIPer/PACEChecks/Checks/CheckPACE.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KipTM.Archive;
+using KipTM.Model.Checks;
+
+namespace PACEChecks
+{
+    public class CheckPACE : ICheckMethod
+    {
+        private string _key = "PACECheck";
+
+        #region ICheckMethod implimentation
+
+        public string Key
+        {
+            get { return _key; }
+        }
+
+        public string Title { get { return "Поверка PACE"; } }
+
+        public object GetCustomConfig(IPropertyPool propertyPool)
+        {
+            return null;
+        }
+
+        public bool Init(object customConf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CheckStepConfig> Steps { get; private set; }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+    }
+}
