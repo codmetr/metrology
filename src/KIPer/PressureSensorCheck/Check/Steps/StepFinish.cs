fa8a08875db349ff630557b9ccb139f491d7bb03
codmetr <cod.metr@yandex.ru>
2017-08-23 00:01:10 +0300

    описаны все три шага необходима инициализация и настройка

--- src/KIPer/PressureSensorCheck/Check/Steps/StepFinish.cs
+++ src/KIPer/PressureSensorCheck/Check/Steps/StepFinish.cs
@@ -6,15 +6,33 @@ using System.Threading;
 using System.Threading.Tasks;
 using KipTM.Interfaces.Checks.Steps;
 using KipTM.Model.Channels;
+using PressureSensorCheck.Data;
 
 namespace PressureSensorCheck.Check.Steps
 {
+    /// <summary>
+    /// Финальный шаг проверки
+    /// </summary>
     class StepFinish: TestStep
     {
+        /// <summary>
+        /// Базовая точка
+        /// </summary>
+        public PressureConverterPoint _pointBase;
+
+        /// <summary>
+        /// Пользовательский канал
+        /// </summary>
         private IUserChannel _userChannel;
+
         public override void Start(CancellationToken cancel)
         {
-            throw new NotImplementedException();
+            _userChannel.Message = $"Установите на эталонном источнике давления значение {_pointBase.PressurePoint} {_pointBase.PressureUnit}";
+            var wh = new ManualResetEvent(false);
+            _userChannel.NeedQuery(UserQueryType.GetAccept, wh);
+            WaitHandle.WaitAny(new[] { wh, cancel.WaitHandle });
+            if (cancel.IsCancellationRequested)
+                return;
         }
     }
 }
