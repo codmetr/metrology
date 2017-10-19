bf83c911841b62fe1f9ed8b96f37b8df704a2c66
codmetr <cod.metr@yandex.ru>
2017-10-20 00:03:07 +0300

    прототип РЭ

---
+++ src/KIPer/KIPer/Checks/View/PointConfigConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using KipTM.Checks.ViewModel.Config;
+
+namespace KipTM.Checks.View
+{
+    [ValueConversion(typeof(PointConfigViewModel), typeof(string))]
+    public class PointConfigConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(string))
+                throw new InvalidOperationException("The target must be a string");
+            var conf = value as PointConfigViewModel;
+            if (conf == null)
+                return string.Empty;
+            return $"{conf.Pressire} {conf.Unit}";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
