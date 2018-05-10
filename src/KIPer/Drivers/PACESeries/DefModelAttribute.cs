32cc826aaa7d71777262523dde814498624dd9d1
codmetr <cod.metr@ya.ru>
2018-05-10 20:55:19 +0300

    недокоммиченные куски

---
+++ src/KIPer/Drivers/PACESeries/DefModelAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PACESeries
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    class DefModelAttribute:Attribute
+    {
+        public readonly Model Model;
+
+        /// <summary>
+        /// Описывает с какими моделями доступна функция
+        /// </summary>
+        /// <param name="model"></param>
+        public DefModelAttribute(Model model)
+        {
+            Model = model;
+        }
+    }
+}
---
+++ src/KIPer/Drivers/PACESeries/Model.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PACESeries
+{
+    [Flags]
+    internal enum Model
+    {
+        PACE1000 = 1 >> 0,
+        PACE5000 = 1 >> 1,
+        PACE6000 = 1 >> 2,
+    }
+}
